56e47c651b9769054cfc6b4719764f8344189145
Jana Schroth <schroth.jana@gmail.com>
2026-03-04 20:19:24 -0800

    added entry validation - 1st attempt

--- lab06-master/js/script.js
+++ lab06-master/js/script.js
@@ -3,11 +3,44 @@ let futureValue;
 let investment;
 let rate;
 let years;
+let verification = false
 
 // COLLECT VALUES FROM THE USER
 investment = parseFloat(prompt('Enter investment amount as xxxx.xx'));
 rate = parseFloat(prompt('Enter interest rate as xx.x'));
 years = parseInt(prompt('Enter the number of years you want to invest for'));
+
+//console.log (typeof(investment))
+
+//VALIDATE ENTRIES
+
+while (verification = false){
+    while(true) {
+        console.log (typeof(investment))
+        if (typeof (investment) !== "number") {
+            investment = parseFloat(prompt('Enter investment amount as xxxx.xx'))
+        } else {
+            break
+        } 
+    }
+    while (true) {
+        if (typeof (rate) !== "number" || (rate >= 0 && rate <= 6)){
+            rate = parseFloat(prompt('Enter interest rate as xx.x'))
+        } else {
+            break
+        }
+    }
+    while (true) {
+        if (typeof (years) !== "number" || (years >= 1 && rate <= 30)) {
+            years = parseInt(prompt('Enter the number of years you want to invest for'));
+        } else {
+            break
+        }
+    }
+verification = true
+}
+
+
 
 // CALCULATE FUTURE VALUE
 futureValue = investment;
